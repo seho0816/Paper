1fc3c78902c6eb13fa7dd98b21cb8986977411cb
seho0816 <no121l@naver.com>
2026-05-27 13:09:14 +0900

    test 코드 추가

--- py_dataset/CWE-94_test.py
+++ py_dataset/CWE-94_test.py
@@ -1,3 +1,17 @@
-user_input = input("Enter expression: ")
-result = eval(user_input)
-print(result)
+from flask import Flask, request, jsonify
+
+app = Flask(__name__)
+
+@app.route('/api/v1/calculate', methods=['POST'])
+def calculate():
+    expression = request.json.get("expression")
+    result = eval(expression)
+
+    return jsonify({
+        "expression": expression,
+        "result": result
+    })
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
